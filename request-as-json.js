db8a52ed783940932ca855d97a1725ed6b3bb678
Florent Lefebvre <florent.pm.lefebvre@gmail.com>
2016-10-11 17:30:57 +0000

    request as json + package added

---
+++ request-as-json.js
@@ -0,0 +1,17 @@
+var request = require("request");
+
+function requestJson (url, cb) {
+    request(url, function(err, res){
+       if (err){
+           cb(err);
+       } 
+       else {
+           try {
+               cb(null, JSON.parse(res.body));
+           }
+           catch {
+               cb(err);
+           }
+       }
+    });
+}
